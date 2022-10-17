c6a27aefee050d4dbef9363ba0a799fec44b3ed6
Ahmad Aziz <ahamadaziz7@gmail.com>
2022-10-17 14:31:39 +0500

    Added cors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("dotenv-flow").config();
 require("./db/mongoose");
 const path = require("path");
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 // const cookieParser = require("cookie-parser");
 const CronJob = require("cron").CronJob;
 
@@ -20,13 +20,13 @@ const app = express();
 //Other Mounts
 // app.use(cookieParser());
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_URL,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.static(path.join(__dirname, "../ms-fe/build")));
 
